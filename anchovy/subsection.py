df8c65ee88105ff902db0e26115f050e3eddf84d
Marian Domanski <mdomanski@usgs.gov>
2020-05-26 16:11:14 -0500

    add subsection area method

--- anchovy/subsection.py
+++ anchovy/subsection.py
@@ -18,6 +18,7 @@ class SubSection:
         self._station = np.array(station)
         self._elevation = np.array(elevation)
         self._roughness = float(roughness)
+        self._min_elevation = self._elevation.min()
 
         if not np.all(np.isfinite(self._station)):
             raise ValueError("station must be finite")
@@ -39,3 +40,70 @@ class SubSection:
 
         if not np.all(np.diff(self._station) >= 0):
             raise ValueError("station must be in ascending order")
+
+    def _interp_station(self, s1, e1, s2, e2, e):
+
+        slope = (s2 - s1)/(e2 - e1)
+        return slope * (e - e1) + s1
+
+    def _wp(self, elevation):
+        """Station, elevation arrays of wetted perimeter"""
+
+        if elevation <= self._min_elevation:
+            return np.nan, np.nan
+
+        s = []
+        e = []
+
+        if elevation > self._elevation[0]:
+            s.append(self._station[0])
+            e.append(elevation)
+
+        if self._elevation[0] <= elevation:
+            s.append(self._station[0])
+            e.append(self._elevation[0])
+
+        for i in range(1, len(self._station)):
+
+            e1 = self._elevation[i - 1]
+            e2 = self._elevation[i]
+
+            s1 = self._station[i - 1]
+            s2 = self._station[i]
+
+            # in between the previous coordinate and this coordinate
+            if (e1 < elevation and elevation < e2) or \
+                    (e2 < elevation and elevation < e1):
+                e.append(elevation)
+                s.append(self._interp_station(s1, e1, s2, e2, elevation))
+
+            # greater than or equal to this coordinate
+            if e2 <= elevation:
+                e.append(e2)
+                s.append(s2)
+
+        if elevation > self._elevation[-1]:
+            s.append(self._station[-1])
+            e.append(elevation)
+
+        return np.array(s), np.array(e)
+
+    def area(self, elevation):
+
+        elevation = np.array(elevation, dtype=np.float)
+        area = np.empty_like(elevation)
+
+        with np.nditer([elevation, area], [], [['readonly'], ['writeonly']]) \
+                as it:
+            for e, a in it:
+                if e <= self._min_elevation:
+                    a[...] = 0
+                elif not np.isfinite(e):
+                    a[...] = np.nan
+                else:
+                    a[...] = np.trapz(*self._wp(e))
+
+        if area.size == 1:
+            return float(area)
+        else:
+            return area
