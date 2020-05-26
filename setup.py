486265ff3c97e3ae1c229a56b39d066161152d56
Marian Domanski <mdomanski@usgs.gov>
2020-05-26 10:39:25 -0500

    add project files

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+
+try:
+    from sphinx.setup_command import BuildDoc
+    sphinx_imported = True
+except BaseException:
+    sphinx_imported = False
+
+name = 'anchovy'
+version = '0.0'
+release = '0.0.1'
+dev_status = 'Development Status :: 1 - Planning'
+
+install_requires = ['matplotlib', 'numpy']
+
+setup_kwargs = {
+    'name': name,
+    'version': release,
+    'packages': ['anchovy'],
+    'url': 'https://code.usgs.gov/mdomanski/aluminiumanchovy',
+    'license': 'License :: Public Domain',
+    'author': 'Marian Domanski',
+    'author_email': 'mdomanski@usgs.gov',
+    'description': 'Hydraulic geometry',
+    'classifiers': [
+        dev_status,
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Hydrology'
+    ],
+    'python_requires': '~=3.7',
+    'install_requires': install_requires
+}
+
+if sphinx_imported:
+    cmdclass = {'build_sphinx': BuildDoc}
+    docs_source = 'docs/'
+    docs_build_dir = 'docs/_build'
+    docs_builder = 'html'
+    setup_kwargs['command_options'] = {'build_sphinx': {
+        'project': ('setup.py', name),
+        'version': ('setup.py', version),
+        'release': ('setup.py', release),
+        'source_dir': ('setup.py', docs_source),
+        'build_dir': ('setup.py', docs_build_dir),
+        'builder': ('setup.py', docs_builder)}}
+
+setup(**setup_kwargs)
