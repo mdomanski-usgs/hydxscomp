907b858c9e83361db76c0268f2fd4e915a41c193
Marian Domanski <mdomanski@usgs.gov>
2020-06-01 12:11:48 -0500

    refactor, add section array

--- anchovy/crosssection.py
+++ anchovy/crosssection.py
@@ -1,35 +1,32 @@
 import numpy as np
 
 
-class SubSection:
-    """Cross section subsection
+class SectionArray:
+    """Section coordinate array
 
     Parameters
     ----------
     station : array_like
-        Station (lateral) coordinate
+        Station (lateral) coordinates. Must be in ascending order,
+        one-dimensional, and the same size as `elevation`.
     elevation : array_like
-        Elevation (vertical) coordinate
-    roughness : float
-        Manning's roughness coefficient, in s/m**(1/3)
+        Elevation (vertical) coordinates. Must be one-dimensional
+        and the same size as `station`.
 
     """
 
-    def __init__(self, station, elevation, roughness):
+    def __init__(self, station, elevation):
 
         self._station = np.array(station)
         self._elevation = np.array(elevation)
-        self._roughness = float(roughness)
         self._min_elevation = self._elevation.min()
+        self._max_elevation = self._elevation.max()
 
         if not np.all(np.isfinite(self._station)):
             raise ValueError("station must be finite")
 
         if not np.all(np.isfinite(self._elevation)):
             raise ValueError("elevation must be finite")
-
-        if not np.isfinite(self._roughness):
-            raise ValueError("roughness must be finite")
 
         if not self._station.ndim == 1:
             raise ValueError("station must be one dimensional")
@@ -43,22 +40,22 @@ class SubSection:
         if not np.all(np.diff(self._station) >= 0):
             raise ValueError("station must be in ascending order")
 
+    @staticmethod
+    def _interp_elevation(s1, e1, s2, e2, s):
+        slope = (e2 - e1)/(s2 - s1)
+        return slope * (s - s2) + e2
+
+    def _interp_station(self, s1, e1, s2, e2, e):
+
+        slope = (s2 - s1)/(e2 - e1)
+        return slope * (e - e1) + s1
+
     def _area(self, elevation):
 
-        s, e = self._sub_array(elevation, 'wa')
-        nan_e = np.isnan(e)
-        return np.trapz(s[~nan_e], e[~nan_e])
-
-    def _top_width(self, elevation):
-        s, e = self._sub_array(elevation, 'wa')
-        tw = 0
-        for i in range(1, len(s)):
-            if np.isnan(e[i-1]) or np.isnan(e[i]):
-                continue
-            else:
-                tw += s[i] - s[i-1]
-
-        return tw
+        sub_array = self._sub_array(elevation, 'a')
+        nan_e = np.isnan(sub_array._elevation)
+        return np.trapz(sub_array._station[~nan_e],
+                        sub_array._elevation[~nan_e])
 
     def _array_comp(self, elevation, func):
 
@@ -80,18 +77,20 @@ class SubSection:
         else:
             return val
 
-    def _conveyance(self, elevation):
+    def _perimeter(self, elevation):
+        sub_array = self._sub_array(elevation, 'p')
 
-        area = self._area(elevation)
-        wetted_perimeter = self._wetted_perimeter(elevation)
-        hydraulic_radius = area / wetted_perimeter
+        wp = 0
 
-        return 1.486/self._roughness * hydraulic_radius**(2/3) * area
+        for i in range(1, len(sub_array._station)):
+            if np.isnan(sub_array._elevation[i-1]) \
+                    or np.isnan(sub_array._elevation[i]):
+                continue
+            wp += \
+                np.sqrt((sub_array._station[i]-sub_array._station[i-1])**2 +
+                        (sub_array._elevation[i]-sub_array._elevation[i-1])**2)
 
-    def _interp_station(self, s1, e1, s2, e2, e):
-
-        slope = (s2 - s1)/(e2 - e1)
-        return slope * (e - e1) + s1
+        return wp
 
     def _sub_array(self, elevation, array_type):
         """Computes and returns sub arrays from station and elevation
@@ -100,12 +99,17 @@ class SubSection:
         ----------
         elevation : float
             Elevation for computing sub arrays
-        array_type : {'wp', 'wa'}
+        array_type : {'p', 'a'}
             Type of computation. Wetted perimeter or wetted area.
+
+        Returns
+        -------
+        SectionArray
 
         """
 
-        assert array_type == 'wp' or array_type == 'wa'
+        if array_type != 'p' and array_type != 'a':
+            raise ValueError("Invalid array type: {}".format(array_type))
 
         if elevation <= self._min_elevation:
             return np.nan, np.nan
@@ -113,7 +117,7 @@ class SubSection:
         s = []
         e = []
 
-        if array_type == 'wa':
+        if array_type == 'a':
             if elevation > self._elevation[0]:
                 s.append(self._station[0])
                 e.append(elevation)
@@ -145,7 +149,7 @@ class SubSection:
                 s.append(s[-1])
                 e.append(np.nan)
 
-        if array_type == 'wa':
+        if array_type == 'a':
             if elevation > self._elevation[-1]:
                 s.append(self._station[-1])
                 e.append(elevation)
@@ -154,20 +158,197 @@ class SubSection:
             s.pop()
             e.pop()
 
-        return np.array(s), np.array(e)
+        cls = self.__class__
+        result = cls.__new__(cls)
+        result._station = np.array(s)
+        result._elevation = np.array(e)
 
-    def _wetted_perimeter(self, elevation):
+        return result
 
-        s, e = self._sub_array(elevation, 'wp')
-
-        wp = 0
-
-        for i in range(1, len(s)):
-            if np.isnan(e[i-1]) or np.isnan(e[i]):
+    def _top_width(self, elevation):
+        sub_array = self._sub_array(elevation, 'a')
+        tw = 0
+        for i in range(1, len(sub_array._station)):
+            if np.isnan(sub_array._elevation[i-1]) \
+                    or np.isnan(sub_array._elevation[i]):
                 continue
-            wp += np.sqrt((s[i]-s[i-1])**2 + (e[i]-e[i-1])**2)
+            else:
+                tw += sub_array._station[i] - sub_array._station[i-1]
 
-        return wp
+        return tw
+
+    def area(self, elevation):
+        """Computes area of this array
+
+        Parameters
+        ----------
+        elevation : array_like
+            Elevation to compute area.
+
+        Returns
+        -------
+        area : float or ndarray
+
+        """
+
+        return self._array_comp(elevation, self._area)
+
+    def copy(self):
+        """Returns a copy of this array
+
+        Returns
+        -------
+        array : SectionArray
+
+        """
+
+        return self.__class__(self._station, self._elevation)
+
+    def max_elevation(self):
+        """Returns the maximum elevation of this array
+
+        Returns
+        -------
+        max_elevation : float
+
+        """
+
+        return self._max_elevation
+
+    def max_station(self):
+        """Returns the maximum station of this array
+
+        Returns
+        -------
+        max_station : float
+
+        """
+
+        return self._station.max()
+
+    def min_elevation(self):
+        """Returns the minimum elevation of this array
+
+        Returns
+        -------
+        min_elevation : float
+
+        """
+
+        return self._min_elevation
+
+    def min_station(self):
+        """Returns the minimum station of this array
+
+        Returns
+        -------
+        min_station : float
+
+        """
+
+        return self._station.min()
+
+    def perimeter(self, elevation):
+        """Returns the perimeter of this array
+
+        Parameters
+        ----------
+        elevation : array_like
+            Elevation for computing the perimeter.
+
+        Returns
+        -------
+        perimeter : float or ndarray
+
+        """
+
+        return self._array_comp(elevation, self._perimeter)
+
+    def split(self, sect_stat):
+
+        split_station = []
+        split_elevation = []
+
+        j = 0
+        s = []
+        e = []
+
+        if sect_stat.ndim == 0:
+            sect_stat = sect_stat[np.newaxis]
+
+        for i in range(len(sect_stat)):
+
+            while self._station[j] < sect_stat[i]:
+                s.append(self._station[j])
+                e.append(self._elevation[j])
+                j += 1
+
+            s.append(sect_stat[i])
+            e_interp = self._interp_elevation(self._station[j-1],
+                                              self._elevation[j-1],
+                                              self._station[j],
+                                              self._elevation[j],
+                                              sect_stat[i])
+            e.append(e_interp)
+
+            split_station.append(np.array(s))
+            split_elevation.append(np.array(e))
+
+            if sect_stat[i] != self._station[j]:
+                s = [split_station[-1][-1]]
+                e = [split_elevation[-1][-1]]
+            else:
+                s = []
+                e = []
+
+        # finish the rest of the station, elevation arrays
+        while j < len(self._station):
+            s.append(self._station[j])
+            e.append(self._elevation[j])
+            j += 1
+
+        split_station.append(np.array(s))
+        split_elevation.append(np.array(e))
+
+        return [SectionArray(s, e) for (s, e)
+                in zip(split_station, split_elevation)]
+
+    def top_width(self, elevation):
+        """Returns the top width of this array
+
+        Parameters
+        ----------
+        elevation : array_like
+            Elevation for computing the top width.
+
+        Returns
+        -------
+        top_width : float or ndarray
+        """
+
+        return self._array_comp(elevation, self._top_width)
+
+
+class SubSection:
+    """Cross section subsection
+
+    Parameters
+    ----------
+    section_array : SectionArray
+    roughness : float
+        Manning's roughness coefficient, in s/m**(1/3)
+
+    """
+
+    def __init__(self, section_array, roughness):
+
+        self._array = section_array.copy()
+
+        self._roughness = float(roughness)
+        self._min_elevation = self._array.min_elevation()
+
+        if not np.isfinite(self._roughness):
+            raise ValueError("roughness must be finite")
 
     def area(self, elevation):
         """Computes wetted area of this subsection
@@ -183,7 +364,7 @@ class SubSection:
 
         """
 
-        return self._array_comp(elevation, self._area)
+        return self._array.area(elevation)
 
     def conveyance(self, elevation):
         """Computes conveyance for this subsection
@@ -199,7 +380,11 @@ class SubSection:
 
         """
 
-        return self._array_comp(elevation, self._conveyance)
+        area = self._array.area(elevation)
+        wetted_perimeter = self._array.perimeter(elevation)
+        hydraulic_radius = area / wetted_perimeter
+
+        return 1.486/self._roughness * hydraulic_radius**(2/3) * area
 
     def top_width(self, elevation):
         """Computes top width of this subsection
@@ -215,7 +400,7 @@ class SubSection:
 
         """
 
-        return self._array_comp(elevation, self._top_width)
+        return self._array.top_width(elevation, self._array.top_width)
 
     def wetted_perimeter(self, elevation):
         """Computes wetted perimeter of this subsection
@@ -231,7 +416,7 @@ class SubSection:
 
         """
 
-        return self._array_comp(elevation, self._wetted_perimeter)
+        return self._array.perimeter(elevation)
 
 
 class CrossSection:
@@ -258,42 +443,23 @@ class CrossSection:
 
     def __init__(self, station, elevation, roughness, sect_stat=None):
 
-        self._station = np.array(station, dtype=np.float)
-        self._elevation = np.array(elevation, dtype=np.float)
+        self._array = SectionArray(station, elevation)
 
         roughness = np.array(roughness, dtype=np.float)
 
         if sect_stat is not None:
             sect_stat = np.array(sect_stat, dtype=np.float)
 
-        if not np.all(np.isfinite(self._station)):
-            raise ValueError("station must be finite")
-
-        if not np.all(np.isfinite(self._elevation)):
-            raise ValueError("elevation must be finite")
-
         if not np.all(np.isfinite(roughness)):
             raise ValueError("roughness must be finite")
-
-        if not self._station.ndim == 1:
-            raise ValueError("station must be one dimensional")
-
-        if not self._elevation.ndim == 1:
-            raise ValueError("elevation must be one dimensional")
-
-        if self._station.size != self._elevation.size:
-            raise ValueError("station and elevation must have the same size")
-
-        if not np.all(np.diff(self._station) >= 0):
-            raise ValueError("station must be in ascending order")
 
         if roughness.size > 1:
             if sect_stat is None:
                 raise ValueError("rough_stat cannot be None")
             if roughness.size - 1 != sect_stat.size:
                 raise ValueError("Invalid number of rough_stat values")
-            if sect_stat.min() <= self._station.min() \
-                    or self._station.max() <= sect_stat.max():
+            if sect_stat.min() <= self._array.min_station() \
+                    or self._array.max_station() <= sect_stat.max():
                 raise ValueError(
                     "rough_stat bounds must be inside station bounds")
             if sect_stat.size > 1:
@@ -306,75 +472,22 @@ class CrossSection:
             self._subsections = self._sections(
                 station, elevation, roughness, sect_stat)
         else:
-            self._subsections = [SubSection(station, elevation, roughness)]
-
-    @staticmethod
-    def _interp_elevation(s1, e1, s2, e2, s):
-        slope = (e2 - e1)/(s2 - s1)
-        return slope * (s - s2) + e2
+            array = SectionArray(station, elevation)
+            self._subsections = [SubSection(array, roughness)]
 
     def _sections(self, station, elevation, roughness, rough_stat):
 
         sections = []
 
-        split_station, split_elevation = self._split_arrays(
-            station, elevation, rough_stat)
+        split_arrays = self._array.split(rough_stat)
 
         for i, n in enumerate(roughness):
-            sections.append(SubSection(
-                split_station[i], split_elevation[i], n))
+            sections.append(SubSection(split_arrays[i], n))
 
         return sections
 
-    @classmethod
-    def _split_arrays(cls, station, elevation, sect_stat):
-
-        split_station = []
-        split_elevation = []
-
-        j = 0
-        s = []
-        e = []
-
-        if sect_stat.ndim == 0:
-            sect_stat = sect_stat[np.newaxis]
-
-        for i in range(len(sect_stat)):
-
-            while station[j] < sect_stat[i]:
-                s.append(station[j])
-                e.append(elevation[j])
-                j += 1
-
-            s.append(sect_stat[i])
-            e_interp = cls._interp_elevation(station[j-1], elevation[j-1],
-                                             station[j], elevation[j],
-                                             sect_stat[i])
-            e.append(e_interp)
-
-            split_station.append(np.array(s))
-            split_elevation.append(np.array(e))
-
-            if sect_stat[i] != station[j]:
-                s = [split_station[-1][-1]]
-                e = [split_elevation[-1][-1]]
-            else:
-                s = []
-                e = []
-
-        # finish the rest of the station, elevation arrays
-        while j < len(station):
-            s.append(station[j])
-            e.append(elevation[j])
-            j += 1
-
-        split_station.append(np.array(s))
-        split_elevation.append(np.array(e))
-
-        return split_station, split_elevation
-
     def area(self, elevation):
-        """Area computed for this cross section
+        """Computes area for this cross section
 
         Parameters
         ----------
@@ -387,12 +500,7 @@ class CrossSection:
 
         """
 
-        area = 0
-
-        for ss in self._subsections:
-            area += ss.area(elevation)
-
-        return area
+        return self._array.area(elevation)
 
     def conveyance(self, elevation):
         """Computes conveyance for this cross section
@@ -429,12 +537,7 @@ class CrossSection:
 
         """
 
-        top_width = 0
-
-        for ss in self._subsections:
-            top_width += ss.top_width(elevation)
-
-        return top_width
+        return self._array.top_width(elevation)
 
     def velocity_coeff(self, elevation):
         """Computes velocity coefficient (alpha) for this cross section
@@ -494,9 +597,4 @@ class CrossSection:
 
         """
 
-        wp = 0
-
-        for ss in self._subsections:
-            wp += ss.wetted_perimeter(elevation)
-
-        return wp
+        return self._array.perimeter(elevation)
