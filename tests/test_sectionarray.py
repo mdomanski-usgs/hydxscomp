734f8b2b2187f1ca1e13aa6840abbadb78742ad1
Marian Domanski <mdomanski@usgs.gov>
2020-09-25 14:14:59 -0500

    move section array to new module

--- tests/test_sectionarray.py
+++ tests/test_sectionarray.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 import numpy as np
 
-from anchovy.crosssection import SectionArray
+from anchovy.sectionarray import SectionArray
 
 
 class TestSectionArray(TestCase):
